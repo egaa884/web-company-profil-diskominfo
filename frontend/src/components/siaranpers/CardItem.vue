7892b2c6893541c55245e4ad828fb19e084a641e
afiga rohman <afigarohman14@gmail.com>
2025-08-12 23:18:24 +0700

    feat: tambah halaman siaran pers

---
+++ frontend/src/components/siaranpers/CardItem.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="property-card">
+    <div class="property-image" :style="{ backgroundImage: 'url(' + cardData.imageUrl + ')' }">
+      </div>
+    <div class="property-description">
+      <h5>{{ cardData.title }}</h5>
+      <p>{{ cardData.description }}</p>
+    </div>
+    <a href="#">
+      <div class="property-social-icons">
+        </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardItem',
+  props: {
+    cardData: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*
+  ==========================================
+  Kode CSS dari wpdean.com
+  ==========================================
+*/
+.property-card
+{
+  height:18em;
+  width:14em;
+  display:flex;
+  flex-direction:column;
+  position:relative;
+  transition:all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+  border-radius:16px;
+  overflow:hidden;
+  box-shadow:  15px 15px 27px #e1e1e3, -15px -15px 27px #ffffff;
+}
+
+.property-image
+{
+  height:6em;
+  width:14em;
+  padding:1em 2em;
+  position:absolute;
+  top:0px;
+  transition:all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+  background-size:cover;
+  background-repeat:no-repeat;
+  background-position: center;
+}
+
+.property-description
+{
+  background-color: #FAFAFC;
+  height:12em;
+  width:14em;
+  position:absolute;
+  bottom:0em;
+  transition:all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+  padding: 0.5em 1em;
+  text-align:center;
+}
+
+h5
+{
+  margin:0px;
+  font-size:1.4em;
+  font-weight:700;
+}
+
+p
+{
+  font-size:12px;
+}
+
+.property-social-icons
+{
+  width:1em;
+  height:1em;
+  background-color:black;
+  position:absolute;
+  bottom:1em;
+  left:1em;
+  transition:all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+
+.property-card:hover .property-description
+{
+  height:0em;
+  padding:0px 1em;
+}
+
+.property-card:hover .property-image
+{
+  height:18em;
+}
+
+.property-card:hover .property-social-icons
+{
+  background-color:white;
+}
+
+.property-card:hover .property-social-icons:hover
+{
+  background-color:blue;
+  cursor:pointer;
+}
+</style>
---
+++ frontend/src/views/SiaranPers.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="card-page">
+    <div class="card-container">
+      <CardItem 
+        v-for="card in cards" 
+        :key="card.id" 
+        :cardData="card" 
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardItem from '../components/siaranpers/CardItem.vue'; // Sesuaikan path jika folder CardItem berbeda
+
+// Impor gambar dari folder assets
+import image1 from '@/assets/img/1.jpeg';
+import image2 from '@/assets/img/2.jpeg';
+import image3 from '@/assets/img/3.jpg';
+import image4 from '@/assets/img/4.jpg';
+
+export default {
+  name: 'SiaranPers',
+  components: {
+    CardItem,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          id: 1,
+          title: 'Pengenalan Keterbukaan Informasi Publik',
+          description: 'Diskominfostandi Kota Depok menggelar sosialisasi Pengenalan Keterbukaan Informasi Publik (KIP) dengan tema “Pola Interaksi dan Pola Komunikasi di Era Digital” di Balai Kota Depok pada 26 Juli 2023.',
+          imageUrl: image1,
+        },
+        {
+          id: 2,
+          title: 'Gerakan Masyarakat Hidup Sehat',
+          description: 'Diskominfostandi Kota Depok bersama perangkat daerah lainnya turut menyukseskan Gerakan Masyarakat Hidup Sehat dengan 15.000 peserta senam di Lapangan Balai Kota Depok.',
+          imageUrl: image2,
+        },
+        {
+          id: 3,
+          title: 'Peringatan Hari Pers Nasional 2024',
+          description: 'Walikota Depok, Mohammad Idris, secara simbolis menyerahkan piagam penghargaan dan santunan kepada 5 orang jurnalis Kota Depok saat acara Peringatan Hari Pers Nasional 2024.',
+          imageUrl: image3,
+        },
+        {
+          id: 4,
+          title: 'Penyerahan Santunan Satlinmas Program JKK-JKM',
+          description: 'Diskominfostandi Kota Depok menyerahkan santunan Jaminan Kematian (JKK) kepada 28 orang Satuan Perlindungan Masyarakat (Satlinmas) di Aula Balai Kota Depok.',
+          imageUrl: image4,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.card-page {
+    margin-top: 5%;
+  font-family: 'RobotoDraft', 'Roboto', sans-serif;
+  background-color: #f2f2f2;
+  padding: 2rem;
+}
+
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2rem;
+  justify-items: center;
+}
+</style>
