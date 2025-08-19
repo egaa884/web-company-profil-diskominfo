f451944d66b949b0cb351ec0c4825919fbbc0361
afiga rohman <afigarohman14@gmail.com>
2025-08-19 15:40:31 +0700

    feat(survei): tambah halaman Survei Kepuasan

---
+++ frontend/src/views/SurveiKepuasan.vue
@@ -0,0 +1,24 @@
+<template>
+    <HeroSection 
+      firstLine="SURVEI"
+      secondLine="KEPUASAN MASYARAKAT"
+      thirdLine="DISKOMINFO KOTA MADIUN"
+      backgroundImage="@/assets/img/logo-kominfo.png"
+    />
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center py-20">
+    <h1 class="text-3xl font-bold mb-10">Survei Kepuasan Masyarakat</h1>
+
+    <!-- Sertifikat Card 1 -->
+    <SertifikatCard 
+      :image="SertifImage"
+      pdf="/sertifikat.pdf"
+      title="Survei Kepuasan Masyarakat 2025"
+    />
+  </div>
+</template>
+
+<script setup>
+import SertifikatCard from "@/components/global/SertifikatCard.vue";
+import HeroSection from "@/components/global/HeroSection.vue";
+import SertifImage from "@/assets/img/sertif1.0.png"
+</script>
